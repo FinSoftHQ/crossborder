94deb46be3564b9a5a867875318eecd6d92a524b
AhLei <138641969+9Kittipich@users.noreply.github.com>
2025-04-07 16:50:20 +0700

    Update links in ecommerce pages to use the homepage

--- app/pages/ecommerce/orderSummary.vue
+++ app/pages/ecommerce/orderSummary.vue
@@ -196,7 +196,7 @@
                                     </dd>
                                 </dl>
                             </div>
-                            <!-- 
+                            <!--
                         <div class="flex items-start sm:items-center">
                             <input id="terms-checkbox-2"
                                    type="checkbox"
@@ -212,7 +212,7 @@
                         </div> -->
 
                             <div class="grid grid-cols-2 gap-4">
-                                <UButton to="/ecommerce/products"
+                                <UButton to="/"
                                          class="flex w-full items-center justify-center rounded-lg  border border-gray-200 bg-white px-5  py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
                                     กลับไปยังหน้าสินค้า
                                 </UButton>
