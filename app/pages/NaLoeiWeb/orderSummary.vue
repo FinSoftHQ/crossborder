87cfa8c759b923b13c9d06d1fd670a85efa84607
Jane1m <rinda@finsoft2023.com>
2025-04-09 17:38:07 +0700

    Update button text from 'Order Product' to 'Book Room' in order summary

--- app/pages/NaLoeiWeb/orderSummary.vue
+++ app/pages/NaLoeiWeb/orderSummary.vue
@@ -231,7 +231,7 @@
                                 </UButton>
                                 <UButton to="/ecommerce/payment"
                                          class="flex w-full items-center justify-center rounded-lg bg-primary-700  px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300  dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:mt-0">
-                                    สั่งสินค้า
+                                    จองห้องพัก
                                 </UButton>
                             </div>
                         </div>
